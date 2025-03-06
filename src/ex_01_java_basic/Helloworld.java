5f727c26c1daa4155c3b26983bbd82c68e611f5e
Shivanisohal <sohalshivani2@gmail.com>
2025-03-06 23:15:57 +0530

    6th March, Pushing First Test Code

---
+++ src/ex_01_java_basic/Helloworld.java
@@ -0,0 +1,9 @@
+package ex_01_java_basic;
+
+public class Helloworld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World, First Program");
+    }
+
+}
---
+++ src/ex_01_java_basic/Lab001_HelloWorldProgram.java
@@ -0,0 +1,7 @@
+package ex_01_java_basic;
+
+public class Lab001_HelloWorldProgram {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
