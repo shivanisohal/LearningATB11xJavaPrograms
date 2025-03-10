3bb31055381b78084f92330e023af151b49ef2e3
Shivanisohal <sohalshivani2@gmail.com>
2025-03-10 09:00:17 +0530

    10th March Program

---
+++ src/ex_01_java_basic/PrintEnteredIntegerValue.java
@@ -0,0 +1,17 @@
+package ex_01_java_basic;
+
+
+import java.util.Scanner;
+
+public class PrintEnteredIntegerValue {
+    public static void main(String[] args) {
+        int Num;
+        System.out.println("Enter any Number=");
+        //Scanner obj
+        Scanner n=new Scanner(System.in);
+        // Read the next integer from the screen
+        Num=n.nextInt();
+        System.out.println("Entered number="+Num);
+        n.close();
+    }
+}
