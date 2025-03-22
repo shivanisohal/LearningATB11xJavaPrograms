64a882c29fa9305a0d6a60dcec23a7ec476c2188
Shivanisohal <sohalshivani2@gmail.com>
2025-03-22 21:59:24 +0530

    22th March, FibonacciSeries

---
+++ src/ex_01_java_basic/Lab0016_FibonacciSeries.java
@@ -0,0 +1,23 @@
+package ex_01_java_basic;
+
+import java.util.Scanner;
+
+public class Lab0016_FibonacciSeries {
+    public static void main(String[] args) {
+
+        int n1=0;
+        int n2=1;
+        int fab;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter any number=");
+        int n=sc.nextInt();
+        System.out.print(n1+","+n2+",");
+        for(int i=3;i<=n;i++)
+        {
+fab=n1+n2;
+            System.out.print(","+fab);
+            n1=n2;
+            n2=fab;
+        }
+    }
+}
